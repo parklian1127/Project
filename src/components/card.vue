167979636bc95afed7a563c6033a912cf4a7e852
parklian1127 <piaoxuelian1127@126.com>
2019-05-05 17:19:01 +0800

    全局配置

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,19 +1,11 @@
 <template>
-  <div>
-    <p class="card">
-      {{text}}
-    </p>
-  </div>
+
 </template>
 
 <script>
-export default {
-  props: ['text']
-}
+
 </script>
 
 <style>
-.card {
-  padding: 10px;
-}
+
 </style>
